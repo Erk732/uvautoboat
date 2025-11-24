8bdbf2e22089a0438bc180f99540c4cd62b42cae
Ghostzero00018 <yinpuchen0@gamil.com>
2025-11-24 09:44:00 +0100

    Initialize control package with Apache License, setup configuration, and basic controller script

---
+++ control/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ghostzero',
+    maintainer_email='yinpuchen0@gamil.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'simple_controller = control.simple_controller:main'
+        ],
+    },
+)
