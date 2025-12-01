513eaa55be1deb97aebe5aabce153c0a93a4e300
Ghostzero00018 <yinpuchen0@gamil.com>
2025-12-01 14:51:04 +0100

    Add additional entry points for tf_broadcaster and waypoint_visualizer in setup.py

--- plan/setup.py
+++ plan/setup.py
@@ -29,12 +29,15 @@ setup(
             'simple_perception = brain.simple_perception:main',
             'mission_trigger = brain.mission_trigger:main',
             'tf_broadcaster = brain.tf_broadcaster:main',
+            'tf_broadcaster_gps = brain.tf_broadcaster_gps:main',
+            'tf_broadcaster_gazebo = brain.tf_broadcaster_gazebo:main',
             'apollo11 = brain.apollo11:main',
             'vostok1 = brain.vostok1:main',
+            'waypoint_visualizer = brain.waypoint_visualizer:main',
             # Modular nodes (TNO style names)
             'oko_perception = brain.oko_perception:main',
             'sputnik_planner = brain.sputnik_planner:main',
-            # new code for advanced testing unfortunately still has the control and the planning parts together! 
+            # Atlantis - separated control and planning
             'atlantis = brain.atlantis:main',
             'atlantis_planner = brain.atlantis_planner:main',
         ],
